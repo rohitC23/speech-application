7ecf69e710e573375a979a73234529f708256eab
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-11-14 16:11:46 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -14,7 +14,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/assessment/signup', {
+      const response = await fetch('http://104.155.186.187:5000/assessment/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
