6c05dc148c1f6949d5bf9b57fdc23a968527e6d0
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-11-14 16:11:20 +0530

    Update Sentences.js

--- src/Sentences.js
+++ src/Sentences.js
@@ -125,7 +125,7 @@ function Sentences({ audioFile, question }) {
         formData.append('user_id', user_id);
         formData.append('file', wavBlob, 'recording.wav');
 
-        const response = await fetch('http://127.0.0.1:8000/evaluate_tense', {
+        const response = await fetch('http://104.155.186.187:5000/evaluate_tense', {
           method: 'POST',
           body: formData,
         });
