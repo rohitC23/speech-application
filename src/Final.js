f1fe64400777ae7ddea01b19b0b11a8633752f74
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-11-15 16:13:52 +0530

    Update Final.js

--- src/Final.js
+++ src/Final.js
@@ -32,7 +32,7 @@ function Final() {
 
     const handleContinue = async () => {
         localStorage.setItem('score', []);
-        navigate('/home');
+        navigate('/level-para');
     };
 
     return (
@@ -51,7 +51,7 @@ function Final() {
                         onClick={handleContinue}
                         className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
                     >
-                        Return To Home
+                        Continue
                     </button>
                 </div>
                 
