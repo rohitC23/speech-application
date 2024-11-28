3a4522693550e66371c1f9ec7869fb3971131721
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-11-28 12:38:40 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ function Login() {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/assessment/login', {
+      const response = await fetch('https://communication.theknowhub.com/api/assessment/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
