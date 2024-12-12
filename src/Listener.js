43bdecac01eb4381627f2bd0a9dbeb0bb5729eea
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-12-12 15:21:04 +0530

    Update Listener.js

--- src/Listener.js
+++ src/Listener.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import sampleImage from 'url:./assets/sound.png';
+import React, { useState } from 'react';
 import Header from './Header';
 import Questions from './Questions';
 
@@ -7,14 +6,14 @@ function Listener() {
   const [loading, setLoading] = useState(true);
   const [questions, setQuestions] = useState([]);
   const [popup, setPopup] = useState({ message: '', type: '' });
-  const [isClicked, setIsClicked] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
   const [audioFile, setAudioFile] = useState(null); // State to store the audio file URL and name
 
   const fetchAudioAndQuestions = async () => {
     const user_id = localStorage.getItem('user_id'); // Retrieve the user ID from localStorage
     try {
       // Fetch audio first
-      const audioResponse = await fetch('https://communication.theknowhub.com/api/listening_comprehension_audio', {
+      const audioResponse = await fetch('http://127.0.0.1:8000/listening_comprehension_audio', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -33,7 +32,7 @@ function Listener() {
         setAudioFile({ url: audioFileURL, name: filename });
 
         // Fetch questions only after audio fetch is successful
-        const questionsResponse = await fetch('https://communication.theknowhub.com/api/listening_comprehension', {
+        const questionsResponse = await fetch('http://127.0.0.1:8000/listening_comprehension', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -54,46 +53,65 @@ function Listener() {
         setTimeout(() => setPopup({ message: '', type: '' }), 3000);
       }
     } catch (error) {
+      setPopup({ message: 'An unexpected error occurred', type: 'error' });
+      setTimeout(() => setPopup({ message: '', type: '' }), 3000);
     } finally {
       setLoading(false); // Stop loading spinner
     }
   };
 
-  useEffect(() => {
+  const startListening = () => {
+    setIsStarted(true);
     fetchAudioAndQuestions();
-  }, []);
-
-  const handleClick = () => {
-    const now = new Date();
-    let currentTime = now.toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    });
-    localStorage.setItem('duration', currentTime);
-    setIsClicked(true); // Set isClicked to true
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 pt-20">
-      <Header showNav={true} />
-  
+      <Header showNav={true} hiddenNavItems={['/Home']}/>
+      <div className="flex items-center space-x-4 mb-10">
+        <div className="bg-green-500 text-white rounded-full w-8 h-8 flex items-center justify-center">1</div>
+        <p className="text-green-500">Correct the Sentences</p>
+        <div className="bg-green-500 text-white rounded-full w-8 h-8 flex items-center justify-center">2</div>
+        <p className="text-green-500">Correct the Tenses</p>
+        <div className="bg-blue-500 text-white w-8 h-8 rounded-full flex items-center justify-center">3</div>
+        <p className="text-blue-500">Listening Comprehension</p>
+      </div>
+
       <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-[900px] h-auto flex flex-col justify-center items-center">
         <h2 className="text-2xl font-bold mb-6">Listening Comprehension</h2>
-        <p className="text-md mb-6">
-          {loading
-            ? 'Loading data, please wait...'
-            : 'Listen to the audio carefully then answer the questions'}
-        </p>
-  
-        {!isClicked && (
+        
+        {!audioFile &&(
+          <div> 
+          <p className="text-md mb-6">Prepare yourself to listen to an audio clip and answer the related questions.</p>
+          <p className="text-lg mb-2 font-bold">Instructions</p>
+          <ul className="list-disc list-inside mb-6">
+            <li>Carefully listen the audio provided.</li>
+            <li>Answer the questions based on your understanding.</li>
+            <li>Use context clues.</li>
+            <li>Build summary of the audio.</li>
+          </ul>
+        </div>
+        )}
+
+      <div className="border-t border-gray-300 mb-6 w-full"></div>
+
+        {!isStarted ? (
           <>
-            <img
-              src={sampleImage}
-              alt="Prompt"
-              className="mb-6 w-[100px] h-[100px] mx-auto rounded-lg"
-            />
-  
+            <button
+              onClick={startListening}
+              className="px-8 py-3 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg text-lg"
+            >
+              Start
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="text-md mb-6">
+              {loading
+                ? 'Loading data, please wait...'
+                : 'Listen to the audio carefully then answer the questions'}
+            </p>
+
             {audioFile && (
               <div className="mb-6 w-full">
                 <audio controls src={audioFile.url} className="w-full">
@@ -101,25 +119,15 @@ function Listener() {
                 </audio>
               </div>
             )}
-  
-            <div className="border-t border-gray-300 mb-8 w-full"></div>
-  
-            <button
-              onClick={handleClick} // Call handleClick on button click
-              className="px-8 py-3 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg text-lg"
-            >
-              Generate Questions
-            </button>
+
+            {!loading && <Questions questions={questions} />}
           </>
         )}
-  
-        {/* Render Questions component with questions */}
-        {!loading && isClicked && <Questions questions={questions} />}
       </div>
-  
+
       {popup.message && (
         <div
-          className={`absolute top-20 p-4 rounded-lg text-white shadow-lg ${
+          className={`fixed top-20 left-3/4 flex items-center justify-center w-80 h-20 m-auto rounded-lg text-white shadow-lg ${
             popup.type === 'success' ? 'bg-green-500' : 'bg-red-500'
           }`}
         >
@@ -128,7 +136,6 @@ function Listener() {
       )}
     </div>
   );
-  
 }
 
 export default Listener;
