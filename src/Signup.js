26e227fb4d49d3d281fc5431f741fffb85cb4646
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-12-12 15:23:40 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Signup() {
   const [firstName, setFirstName] = useState('');
@@ -8,13 +9,18 @@ function Signup() {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const [isSuccess, setIsSuccess] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://communication.theknowhub.com/api/assessment/signup', {
+      const response = await fetch('http://127.0.0.1:8000/assessment/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -88,16 +94,28 @@ function Signup() {
               className="mt-1 p-2 border border-gray-300 rounded w-full"
             />
           </div>
-          <div className="mb-4">
+          <div className="mb-4 relative">
             <label className="block text-sm font-medium text-gray-700" htmlFor="password">Password</label>
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
               className="mt-1 p-2 border border-gray-300 rounded w-full"
             />
+            <button 
+              type="button" 
+              onClick={handlePasswordToggle} 
+              className="absolute right-2 top-8" // Position the eye icon
+              aria-label={showPassword ? "Hide password" : "Show password"}
+            >
+              {showPassword ? (
+                <i className="fa fa-eye" aria-hidden="true"></i>
+              ) : (
+                <i className="fa fa-eye-slash" aria-hidden="true"></i>
+              )}
+            </button>
           </div>
           <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Sign Up</button>
         </form>
