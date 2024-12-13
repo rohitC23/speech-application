27fb71122dea4e72e54f7b34552adedc6eea528a
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-12-13 12:27:43 +0530

    Update Questions.js

--- src/Questions.js
+++ src/Questions.js
@@ -43,7 +43,7 @@ function Questions({ questions }) {
 
     try {
       const response = await fetch(
-        'http://127.0.0.1:8000/evaluate_listening_comprehension',
+        'https://communication.theknowhub.com/api/evaluate_listening_comprehension',
         {
           method: 'POST',
           headers: {
@@ -148,7 +148,7 @@ function Questions({ questions }) {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/user/insert/score', {
+      const response = await fetch('https://communication.theknowhub.com/api/user/insert/score', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -179,7 +179,7 @@ function Questions({ questions }) {
 
     try {
       // Send the POST request
-      const response = await fetch('http://127.0.0.1:8000/submit', {
+      const response = await fetch('https://communication.theknowhub.com/api/submit', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
