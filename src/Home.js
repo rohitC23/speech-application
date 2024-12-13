9e36bc71e28eca1b459fb306ce8b0dee88652238
rohitC23 <164848375+rohitC23@users.noreply.github.com>
2024-12-13 12:23:05 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -13,14 +13,14 @@ function Home() {
   
   // Fetch the word of the day when the component mounts
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/generate_word')
+    fetch('https://communication.theknowhub.com/api/generate_word')
       .then((response) => response.json())
       .then((data) => {
         const today = Object.keys(data)[0]; // Get the date
         setWordData(data[today]); // Save the word data
         
         // Fetch the audio file for the word of the day
-        fetch('http://127.0.0.1:8000/generate_word_audio', {
+        fetch('https://communication.theknowhub.com/api/generate_word_audio', {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' }
         })
